396faca75740b59d7c3412dd947078b3eef1e810
MaksymKostielei <maxim.kosteley@gmail.com>
2023-01-13 17:39:06 +0200

    done: task 1-4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// // task 1
 const setSum = () => {
     let sum = 0;
         
@@ -13,7 +14,7 @@ const setSum = () => {
   counter(5);
   counter(228);
 
-
+// // task 2
 const getUpdatedArr = () => {
     let sum = [];
         
@@ -32,4 +33,50 @@ const getUpdatedArr = () => {
 updatedArr(3);
 updatedArr(5);
 updatedArr();
- 
+
+// // task 3
+const getTime = () => {
+  let lastUsedTime = null; 
+      
+  return function(){
+  if (lastUsedTime == null) {
+    lastUsedTime = Date.now();
+
+    return console.log('Enabled');
+  }
+
+  let difference = Math.floor((Date.now() - lastUsedTime) / 1000);
+  lastUsedTime = Date.now();
+
+  return console.log(difference);
+  }
+}
+
+let result = getTime();
+result();
+
+// task 4
+
+let time = 5;
+
+let countdown = setInterval(timer, 1000); 
+
+function timer()
+{  
+  if (time <= 0)
+  {
+     clearInterval(countdown);
+     console.log('Time End');
+     return;
+  }
+
+  let mins = '' + Math.floor(time / 60);
+  let secs = '' + time % 60
+  if (secs < 10) secs = '0' + secs;
+  if (mins < 10) mins = '0' + mins; 
+  time--;
+  
+  console.log(`${mins}:${secs}`); 
+}
+
+timer();
